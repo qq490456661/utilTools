2d1ddc67324b829b974725c6b2cddc4b93305c43
linjunjie <490456661@qq.com>
2017-09-11 14:49:00 +0800

    提交

--- src/main/java/redisUtil/Main.java
+++ src/main/java/redisUtil/Main.java
@@ -1,7 +1,15 @@
 package redisUtil;
 
+import com.alibaba.fastjson.JSON;
 import com.google.gson.JsonObject;
+import org.apache.commons.lang.time.DateUtils;
 import redis.clients.jedis.Jedis;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 /**
  * Created by linjunjie(490456661@qq.com) on 2016/11/29.
@@ -23,7 +31,7 @@ public class Main {
         System.out.println(jedis.get("name"));*/
 
         /** Jedis测试*/
-        RedisCacheManager redisCacheManager = new RedisCacheManager();
+       /* RedisCacheManager redisCacheManager = new RedisCacheManager();
         Kepa kepa = new Kepa();
         kepa.setCertNo("431024343445984999XXXpdpsappfpdf000WPPREOFODFDJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJJK00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000888");
         kepa.setName("林俊杰");
@@ -33,7 +41,53 @@ public class Main {
         //Kepa temp = (Kepa)redisCacheManager.getObject("xx");
         System.out.println(temp);
         System.out.println(temp.getName());
-        System.out.println(temp.getCertNo());
+        System.out.println(temp.getCertNo());*/
+
+        /*//本周的
+        Calendar weekCurrent = Calendar.getInstance();
+        weekCurrent.set(weekCurrent.get(Calendar.YEAR), weekCurrent.get(Calendar.MONTH), weekCurrent.get(Calendar.DATE) - 1, 0, 0, 0);
+        weekCurrent.set(Calendar.MILLISECOND, 0);
+        //星期一0点
+        int day = weekCurrent.get(Calendar.DAY_OF_WEEK);
+        System.out.println(day);
+
+        weekCurrent.set(Calendar.DAY_OF_WEEK, weekCurrent.getFirstDayOfWeek());
+        System.out.println("非周末");
+        weekCurrent.add(Calendar.DATE, 1);
+        Date firstDay = weekCurrent.getTime();
+        //下一个星期一0点
+        weekCurrent.add(Calendar.DATE, 7);
+        Date lastDay = weekCurrent.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.format(firstDay));
+        System.out.println(sdf.format(lastDay));*/
+
+
+        //本周的
+        Calendar weekCurrent = Calendar.getInstance();
+        weekCurrent.set(weekCurrent.get(Calendar.YEAR), weekCurrent.get(Calendar.MONTH), weekCurrent.get(Calendar.DATE), 0, 0, 0);
+        weekCurrent.set(Calendar.MILLISECOND, 0);
+
+        int k = weekCurrent.get(Calendar.DAY_OF_WEEK);
+        System.out.println(k);
+        //星期一0点
+        if(k == 1) {
+            weekCurrent.add(Calendar.DATE, -6);
+        }else{
+            weekCurrent.add(Calendar.DATE, 2-k);
+        }
+        Date firstDay = weekCurrent.getTime();
+        //下一个星期一0点
+        weekCurrent.add(Calendar.DATE, 7);
+        Date lastDay = weekCurrent.getTime();
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        System.out.println(sdf.format(firstDay));
+        System.out.println(sdf.format(lastDay));
+
+
+
+        Long a = 128L,b = 128L;
+        System.out.println(a==b);
 
     }
 }
